0613360503fd22ce82450731be587237652dc862
Anthony Bonventre <abonventre@gmail.com>
2016-05-05 18:05:54 -0400

    Added basic inteface

--- client/js/controllers/mainCtrl.js
+++ client/js/controllers/mainCtrl.js
@@ -6,12 +6,32 @@
 app.controller('mainCtrl', ['$scope', 'dataRobot', function ($scope, dataRobot) {
     var vm = this;
 
+    vm.tickets = {
+      'Today': 0,
+      'Tomorrow': 0,
+      '3 Days': 0,
+      '4 Days' : 0,
+      '5 Days' : 0,
+
+    }
+
     dataRobot.getPrices().then(function (response) {
                 //  console.log(response.data.prices);
                  vm.prices = response.data.prices;
              }, function (error) {
                  vm.status = 'Unable to load posts: ' + error.message;
              });
+
+    vm.changeTicket = function(operation, type){
+      if(operation == "add"){
+        vm.tickets[type]++;
+      }else{
+        if(vm.tickets[type] >= 1){
+          vm.tickets[type]--;
+        }
+      };
+
+    };
 
     vm.test = "testing1234";
 }]);
